fc8402cb8dffb1067198a4cd99d3de267c65c7e0
Aswinx46 <aswinx46@gmail.com>
2025-01-07 11:18:13 +0530

    build conflict in sidebar import imn layout component fixed.

--- Frontend/admin/src/components/sidebar/layout.jsx
+++ Frontend/admin/src/components/sidebar/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom'; // Import Outlet
+import Sidebar from './sidebar';
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-100">
